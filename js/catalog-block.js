7bb29a567b798e9a9fce34d39652bb67808eaa82
dick1y <132031946+dick1y@users.noreply.github.com>
2025-05-23 22:15:05 +0500

    Delete js directory

--- js/catalog-block.js
+++
@@ -1,6 +0,0 @@
-$('.filter-block-show-more').on('click', function() {
-      $('.f_more').toggleClass('f_more--open')
-	  $('.filter-block-show-more__label').toggleClass('filter-block-show-more__label--dn')
-	  $('.filter-block-show-more__label2').toggleClass('filter-block-show-more__label2--active')
-	 $('.filter-block-show-more__arrow').toggleClass('filter-block-show-more__arrow--active')
-});
