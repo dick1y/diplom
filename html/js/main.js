8d1c7a1f1051142fc0e05de1f9314055592c2acd
dick1y <132031946+dick1y@users.noreply.github.com>
2025-05-29 17:02:56 +0500

    Update main.js

--- html/js/main.js
+++ html/js/main.js
@@ -247,3 +247,13 @@ $('.sort_name').click(function() {
     location.reload();
     return false;
 });
+
+
+document.getElementById('themeToggle').addEventListener('click', function() {
+    const currentTheme = document.body.className;
+    if (currentTheme === 'light-theme') {
+        document.body.className = 'dark-theme';
+    } else {
+        document.body.className = 'light-theme';
+    }
+});
